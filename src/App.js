8a3eea9ecc7879ae34145a2acfefa61a377024d4
Rashford <mikearowo@gmail.com>
2023-02-06 16:35:01 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Menu" element={<Menu />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/*" element={<h1>PAGE NOT FOUND</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
